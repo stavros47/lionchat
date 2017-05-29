0dbe94a11c27e6751b361562606264ad61ff4ba6
stavros47 <csd2962@csd.uoc.gr>
2017-05-29 15:45:22 +0300

    minor changes(trying to connect externaly)

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ var port = 3000;
 users = [];
 connections = [];
 
-server.listen(port);
+server.listen(port, "0.0.0.0");
 console.log("Server Running..");
-
+console.log("%s socket(s) connected!", connections.length);
 
 app.use(express.static(__dirname + '/'));
 
@@ -17,13 +17,16 @@ app.use(express.static(__dirname + '/'));
 
 io.sockets.on('connection', function(socket){
   connections.push(socket);
-  console.log("Connected! %s sockets connected", connections.length);
+  console.log("Connected! %s socket(s) connected", connections.length);
 
   //Disconnect
   socket.on('disconnect', function(data){
     connections.splice(connections.indexOf(socket), 1);
-    console.log("Disconnected! %s sockets connected", connections.length);
+    console.log("Disconnected! %s socket(s) connected", connections.length);
   });
-
+  //Send message
+  socket.on('send message', function(data){
+    io.sockets.emit('new message', {msg: data});
+  });
 
 });
